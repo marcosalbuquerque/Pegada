4bde8bc413e8256685f69f25a63cb5bd59e6bfbb
marcosalbuquerque <marcosg.as2232@gmail.com>
2025-12-16 14:18:20 -0300

    commit init

---
+++ Pegada/PegadaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PegadaApp.swift
+//  Pegada
+//
+//  Created by Marcos Albuquerque on 16/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PegadaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
