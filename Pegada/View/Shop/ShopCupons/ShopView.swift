283e515f774b4af6a6f7a23635c4ca5d01ccfb69
Joao Pedro <joaopedreleonel@gmail.com>
2025-12-17 18:50:45 -0300

    Ajuste de cor no cupons

--- Pegada/View/Shop/ShopCupons/ShopView.swift
+++ Pegada/View/Shop/ShopCupons/ShopView.swift
@@ -10,7 +10,7 @@ import SwiftData
 
 struct ShopView: View {
     let currentUserId: UUID
-
+    
     @StateObject var viewModel : ShopViewModel
     
     init(currentUserId: UUID, modelContext: ModelContext, userService : UserService) {
@@ -22,45 +22,53 @@ struct ShopView: View {
         ))
     }
     
-
+    
     var body: some View {
+        
         NavigationStack {
-            ScrollView {
-                VStack {
-                    if let profile = viewModel.userProfile {
-                        CardTotalPoints(profile: profile)
-                    }
-
-                    if viewModel.isLoading {
-                        ProgressView()
-                    } else {
-
-                        ForEach(viewModel.coupons) { coupon in
-                            NavigationLink(
-                                destination: BuyCuponView(
-                                    viewModel: viewModel,
-                                    coupon: coupon
-                                ),
-                                label: {
-                                    CouponComponent(
+            ZStack {
+                // Fundo escuro da tela
+                Color .backGroundPerfil
+                    .ignoresSafeArea()
+                ScrollView {
+                    VStack {
+                        if let profile = viewModel.userProfile {
+                            CardTotalPoints(profile: profile)
+                        }
+                        
+                        if viewModel.isLoading {
+                            ProgressView()
+                        } else {
+                            
+                            ForEach(viewModel.coupons) { coupon in
+                                NavigationLink(
+                                    destination: BuyCuponView(
                                         viewModel: viewModel,
                                         coupon: coupon
-                                    )
-                                    .foregroundStyle(Color(.white))
-
-                                }
-                            )
-
+                                    ),
+                                    label: {
+                                        CouponComponent(
+                                            viewModel: viewModel,
+                                            coupon: coupon
+                                        )
+                                        .foregroundStyle(Color(.white))
+                                        
+                                    }
+                                )
+                                
+                            }
                         }
+                        
                     }
-
                 }
+                .navigationTitle("Cupons")
+                .scrollPosition(id: .constant(0), anchor: .top)
+                .task {
+                    await viewModel.loadData(userId: currentUserId)
+                }
+                
             }
-            .navigationTitle("Cupons")
-            .scrollPosition(id: .constant(0), anchor: .top)
-            .task {
-                await viewModel.loadData(userId: currentUserId)
-            }
+            
         }
     }
 }
